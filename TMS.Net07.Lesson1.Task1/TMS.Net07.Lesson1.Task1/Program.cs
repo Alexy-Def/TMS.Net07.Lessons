f5943e027efb65a6d22c1f31f579738912cdf336
Jok <sasha.golubovich@tut.by>
2021-01-28 22:49:19 +0300

    Hello, World!

---
+++ TMS.Net07.Lesson1.Task1/TMS.Net07.Lesson1.Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace TMS.Net07.Lesson1.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sasha!");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+            Console.ReadLine();
+;        }
+    }
+}
